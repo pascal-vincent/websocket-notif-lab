5a8fd5e30ed06e0ba9965b11206045e7b2aeb0aa
Pascal Vincent <pasvincent@gmail.com>
2016-04-28 23:09:01 +0200

    add openshift env variable for port and ip

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
+const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+// if cloud9 port = process.env.PORT
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(process.env.PORT, function () {
+app.listen(server_port, function () {
   console.log('Test app started!');
 });
