5c82515fadd9f8675a7eb20137e37ff51b88cb7e
Pascal Vincent <pasvincent@gmail.com>
2016-05-17 23:07:42 +0200

    add Topic api

--- assets/js/testws.js
+++ assets/js/testws.js
@@ -1,9 +1,10 @@
+
 io.socket.on('connect', function () {
-    io.socket.get('/alerts', {}, function (alerts) {
-        console.log(JSON.stringify(alerts));
+    io.socket.get('/topic/573a1c323637ed818e373a40', {}, function (topic) {
+        console.log(JSON.stringify(topic));
     });
 
-   io.socket.on('alerts', function (event) {
+   io.socket.on('topic', function (event) {
        console.log('Got event : ' + event.verb);
        console.log('with data : ' + JSON.stringify(event.data));
    })
