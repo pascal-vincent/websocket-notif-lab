b0e0d34cf5d642d1b291bcd71f34e91083e5aac8
Pascal Vincent <pasvincent@gmail.com>
2016-05-29 14:46:03 +0200

    add an unsubscribe action

--- api/controllers/TopicController.js
+++ api/controllers/TopicController.js
@@ -6,6 +6,25 @@
  */
 
 module.exports = {
-	
+    unsubscribe: function (req, res) {
+
+        res.set('Content-Type', 'application/json');
+        var jsonResponse = {};
+
+        if (req && req.params && req.params.name) {
+
+            Topic.find({name: req.params.name}).exec(function(err, topic) {
+                if (err) return res.serverError(err);
+                if (req.isSocket) {
+                    Topic.unsubscribe( req, _.pluck(topic, 'id') );
+                }
+                return res.ok();
+            });
+
+        } else {
+            jsonResponse.msg = 'name param not found';
+        }
+        return res.send(jsonResponse);
+    }
 };
 
